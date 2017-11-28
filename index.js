767deed90b14e4caa1634618f72389e4bb5ed98e
Aidan Clemente <aidanclemente@gmail.com>
2017-11-28 17:05:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,11 @@
 function takeANumber(katzDeliLine, newPerson) {
    katzDeliLine.push(`${newPerson}`);
    return (`Welcome, ${newPerson}. You are number ${katzDeliLine.length} in line.`);
+<<<<<<< HEAD
 };
+=======
+}
+>>>>>>> eb1faf5fd73feb84eea5f814eee7b4531b073b2b
 
 function nowServing(katzDeliLine) {
   let i = 0;
@@ -14,6 +18,7 @@ while (i<katzDeliLine.length){
   if(katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!";
   } else {
+<<<<<<< HEAD
     return (`Currently serving ${katzDeliLine.shift()}.`);
   };
 };
@@ -27,8 +32,13 @@ function currentLine(katzDeliLine) {
     i++;
   }
     if (katzDeliLine.length > 0) {
-      return (`The line is currently:` + line);
+      return (`The line is currently:  ${line}`);
     } else {
       return "The line is currently empty.";
     };
 };
+=======
+    return katzDeliLine.shift();
+  }
+}
+>>>>>>> eb1faf5fd73feb84eea5f814eee7b4531b073b2b
